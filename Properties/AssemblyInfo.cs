a391b6b2e5bb7899d5215e78ab844a9adddaff60
tuggl <tuggl@chromium.org>
2013-03-29 06:59:29 +0000

    why did I not think of this

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Phoenix Emulator")]
+[assembly: AssemblyDescription("Phoenix Emulator is an advanced Habbo emulator.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Otaku")]
+[assembly: AssemblyProduct("Phoenix Emulator")]
+[assembly: AssemblyCopyright("Copyright © Sulake Corporation Oy 2006-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16a80ef7-d964-4992-a09f-3a5b4252994c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.11.0")]
+[assembly: AssemblyFileVersion("3.11.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
